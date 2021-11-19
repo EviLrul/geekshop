b05f77a779e1965104a55daa213d64181d32c8b3
EviLrul <EviLrul@yandex.ru>
2021-11-19 10:58:54 +0300

    вывод динамического контента на страницах index и products

--- mainapp/views.py
+++ mainapp/views.py
@@ -14,12 +14,12 @@ def products(request):
     context = {
         'title': 'geekshop - Товары',
         'products': [
-            {'name': 'Худи черного цвета с монограммами adidas Originals', 'price': 6090, 'pic': 'vendor/img/products/Adidas-hoodie.png'},
-            {'name': 'Синяя куртка The North Face', 'price': 23725},
-            {'name': 'Коричневый спортивный oversized-топ ASOS DESIGN', 'price': 3390},
-            {'name': 'Черный рюкзак Nike Heritage', 'price': 2340},
-            {'name': 'Черные туфли на платформе с 3 парами люверсов Dr Martens 1461 Bex', 'price': 13590},
-            {'name': 'Темно-синие широкие строгие брюки ASOS DESIGN', 'price': 2890},
+            {'name': 'Худи черного цвета с монограммами adidas Originals', 'price': 6090, 'about': 'Мягкая ткань для свитшотов. Стиль и комфорт – это образ жизни.', 'pic': 'vendor/img/products/Adidas-hoodie.png'},
+            {'name': 'Синяя куртка The North Face', 'price': 23725, 'about': 'Гладкая ткань. Водонепроницаемое покрытие. Легкий и теплый пуховый наполнитель.', 'pic': 'vendor/img/products/Blue-jacket-The-North-Face.png'},
+            {'name': 'Коричневый спортивный oversized-топ ASOS DESIGN', 'price': 3390, 'about': 'Материал с плюшевой текстурой. Удобный и мягкий.', 'pic': 'vendor/img/products/Brown-sports-oversized-top-ASOS-DESIGN.png'},
+            {'name': 'Черный рюкзак Nike Heritage', 'price': 2340, 'about': 'Плотная ткань. Легкий материал.', 'pic': 'vendor/img/products/Black-Nike-Heritage-backpack.png'},
+            {'name': 'Черные туфли на платформе с 3 парами люверсов Dr Martens 1461 Bex', 'price': 13590, 'about': 'Гладкий кожаный верх. Натуральный материал.', 'pic': 'vendor/img/products/Black-Dr-Martens-shoes.png'},
+            {'name': 'Темно-синие широкие строгие брюки ASOS DESIGN', 'price': 2890, 'about': 'Легкая эластичная ткань сирсакер Фактурная ткань.', 'pic': 'vendor/img/products/Dark-blue-wide-leg-ASOs-DESIGN-trousers.png'},
         ]
     }
     return render(request, 'mainapp/products.html', context)
