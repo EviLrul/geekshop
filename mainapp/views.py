31ad17944dfc564608faee91f0357d2d02602b25
Serg <EviLrul@yandex.ru>
2021-12-04 20:16:39 +0300

    дз 6 со * шаблон для детализации товара

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,14 +1,13 @@
 from django.shortcuts import render
+from django.views.generic import DetailView
+
 from mainapp.models import Product, ProductCategory
 
 import json
 import os
 
-
-
 MODULE_DIR = os.path.dirname(__file__)
 # Create your views here.
-
 
 def index(request):
     context = {
@@ -25,6 +24,17 @@ def products(request):
         'products': Product.objects.all(),
         'categoreis': ProductCategory.objects.all()
     }
-
     #context['products'] = json.load((open(file_path, encoding='utf-8')))
     return render(request, 'mainapp/products.html', context)
+
+
+class ProductDetail(DetailView):
+    # Контроллер вывода информации о продукте
+    model = Product
+    template_name = 'mainapp/detail.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(ProductDetail, self).get_context_data(**kwargs)
+        product = self.get_object()
+        context['product'] = product
+        return context
